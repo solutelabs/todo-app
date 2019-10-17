835a60a93e07aab22c7ceb6aa93b2773d8ad8d4f
Smit Sonani <smit.sonani@solutelabs.com>
2019-10-17 17:50:53 +0530

    Changed empty list message

--- lib/pages/home/items_list_view.dart
+++ lib/pages/home/items_list_view.dart
@@ -63,7 +63,7 @@ class ItemsListView extends StatelessWidget {
                     return SliverFillRemaining(
                       child: Center(
                         child: Text(
-                          "🎉\nYou've finished!",
+                          "✅\nGet sorted!",
                           textAlign: TextAlign.center,
                           style: Theme.of(context).textTheme.display2,
                         ),
