46075f3ef76053d2c6c75fdf4e60f57c01bcf68f
Milind Mevada <milind.mevada@solutelabs.com>
2019-10-24 16:05:46 +0530

    Signin Viewmodel implemented

---
+++ test/utils/string_utils_test.dart
@@ -0,0 +1,12 @@
+import 'package:checklist/utils/string_utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  test('Email validation', () {
+    expect(isEmailValid('milind.mevada@solutelabs.com'), equals(true));
+    expect(isEmailValid(null), equals(false));
+    expect(isEmailValid('   '), equals(false));
+    expect(isEmailValid('milind.mevadasolutelabs.com'), equals(false));
+    expect(isEmailValid('milind.mevada345@solutelabs.com'), equals(true));
+  });
+}
